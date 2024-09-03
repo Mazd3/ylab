63e443284a265cacee433c64684a458c740d66f5
Mazd3 <haziullin2000@gmail.com>
2024-09-03 16:24:31 +0500

    replace image

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -6,10 +6,7 @@ export const SignIn = () => {
   return (
     <div className={styles["sign-in"]}>
       <div className={styles["sign-in__illustration"]}>
-        <img
-          src="https://images.unsplash.com/photo-1620641788421-7a1c342ea42e?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Illustration"
-        />
+        <img src="/image.png" alt="Illustration" />
       </div>
       <div className={styles["sign-in__form-wrapper"]}>
         <img src="/ylab.svg" alt="" />
